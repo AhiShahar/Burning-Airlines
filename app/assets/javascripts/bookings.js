ea16cbe55fada6ec183e68157a63a9c2e7985b5a
dgjd <dgjdavidson@hotmail.com>
2016-12-14 16:49:21 +1100

    fix seat

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -3,13 +3,13 @@ var app = app || {};
 $(document).ready(function() {
   // app.bookings = new app.Bookings();
   // app.bookings.fetch();
-
-  app.router = new app.Router();
-  Backbone.history.start(); // Pay atterntion to hash fragments
-
   var $seat = $(".col .seat");
   $seat.on("click", function() {
     $(this).toggleClass("selected");
+
   });
+
+  app.router = new app.Router();
+  Backbone.history.start(); // Pay atterntion to hash fragments
 
 });
