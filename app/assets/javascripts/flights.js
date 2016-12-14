67a8f87633c5f15b91822a90640aa2e322951fab
dgjd <dgjdavidson@hotmail.com>
2016-12-14 16:20:48 +1100

    margin top

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -12,4 +12,28 @@ $( document ).ready(function() {
   $seat.on("click", function() {
     $(this).toggleClass("selected");
   });
+
+  var jumpingTuna = function () {
+  $(".airplanecartoon").css({
+    position: "absolute",
+    top: 100,
+    right: 0
+  });
+
+  var animateTuna = function () {
+    $(".airplanecartoon").animate({
+      top: "320px"
+    }, 1900, function () {
+      $(".airplanecartoon").animate({
+        top: "100"
+      }, 1900, function () {
+        animateTuna();
+      });
+    });
+  };
+
+  animateTuna();
+};
+
+jumpingTuna();
 });
